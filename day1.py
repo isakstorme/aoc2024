f22880083080a921b05b03dda1a0ed7ef6e1211f
isakstorme <isak.storme@outlook.com>
2024-12-02 08:25:46 +0100

    slight changes in day1

--- day1.py
+++ day1.py
@@ -17,11 +17,14 @@ result1 = 0
 for i in range(len(xs)):
     result1 += abs(xs[i] - ys[i])
 
-print(result1)
 # End of challenge 1
 result2 = 0
 for x in xs:
-    for y in ys: #Really bad time complexity, probably O(n^2) In actual fact it would be sufficient to stop looking if y >= x
+    for y in ys: #Really bad time complexity, probably O(n^2) In actual fact it would be sufficient to stop looking if y >= x.
         if x == y:
             result2 += x
-print(result2)
+
+f = open("solutions\solution1.txt", "w")
+f.write(str(result1) + ": answer 1 \n")
+f.write(str(result2) + ": answer 2 ")
+f.close()
