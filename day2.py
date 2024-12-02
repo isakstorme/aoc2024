2a03aa959e203d633d874373ca33def7036667e0
isakstorme <isak.storme@outlook.com>
2024-12-02 09:01:14 +0100

    some more refactoring

--- day2.py
+++ day2.py
@@ -1,59 +1,57 @@
 with open("input\input2.txt") as input:
     lines = input.read().splitlines()
-#lines = "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9".splitlines()
-result = 0
-for l in lines:
-    sline = l.split()
+
+
+def solution1(lines):
+    result = 0
+    for l in lines:
+        xs = list(map(int, l.split()))
+        if is_correct(xs):
+            result += 1
+    return result
+
+
+def is_correct(xs):  # xs is a list of ints
     dir = -1
-    errorfound = False
-    if int(sline[0]) > int(sline[1]):
+    error_found = False
+    if xs[0] > xs[1]:  # If the list of numbers decrease dir = 1 else dir = -1
         dir = 1
-    elif int(sline[0]) < int(sline[1]):
+    for i in range(len(xs) - 1):
+        # This could probably be made more readable but checks that the list either increases and decreases and that the difference is 1<=d<=3
+        if (xs[i] - xs[i + 1]) * dir > 3 or (xs[i] - xs[i + 1]) * dir < 1:
+            error_found = True
+            break
+    return not error_found
+
+
+def solution2(lines): 
+    result = 0
+    for l in lines:
+        xs = l.split()
+        xs = list(map(int, xs))
         dir = -1
-    for i in range(len(sline) - 1):
-        if (int(sline[i]) - int(sline[i+1])) * dir > 3 or (int(sline[i]) - int(sline[i+1])) * dir < 1:
-            errorfound = True
-    if not errorfound:
-        result += 1
+        error_found = False
+        if xs[0] > xs[1]:
+            dir = 1
+        for i in range(len(xs) - 1):
+            if (xs[i] - xs[i + 1]) * dir > 3 or (xs[i] - xs[i + 1]) * dir < 1:
+                #If error is detected we check if it is possible to remove the number itself or one of the two adjacent numbers
+                s1 = xs[:i] + xs[i + 1:]
+                s2 = xs[:i + 1] + xs[i + 2:]
+                s3 = xs[:i - 1] + xs[i:]
+                if not (is_correct(s1) or is_correct(s2) or is_correct(s3)):
+                    error_found = True
+                break  # took a long time to realise break is necessary: for example 5 7 4 3 2 could not be detected otherwise becuase the for loop would continue considering the following numbers with the expectation that they go uo.
 
-print(result)
+        if not error_found:
+            result += 1
+    return result
 
 
-# part 2
-def isCorrect(s_list):
-    dir = -1
-    errorfound = False
-    if int(s_list[0]) > int(s_list[1]):
-        dir = 1
-    for i in range(len(s_list) - 1):
-        if (int(s_list[i]) - int(s_list[i+1])) * dir > 3 or (int(s_list[i]) - int(s_list[i+1])) * dir < 1:
-            errorfound = True
-    if not errorfound:
-        return True
-result = 0
-for l in lines:
-    sline = l.split()
-    dir = -1
-    errorfound = False
-    if int(sline[0]) > int(sline[1]):
-        dir = 1
-    for i in range(len(sline) - 1):
-        if (int(sline[i]) - int(sline[i+1])) * dir > 3 or (int(sline[i]) - int(sline[i+1])) * dir < 1:
-            print(l)
-            if len(sline) >= i+1:
-                s1 = sline[:i] + sline[i+1:]
-            if len(sline) >= i+2:
-                s2 = sline[:i+1] + sline[i+2:]
-            if i != 0:
-                s3 = sline[:i-1] + sline[i:]
-            print(s1)
-            print(s2)
-            print(s3)
-            if not (isCorrect(s1) or isCorrect(s2) or isCorrect(s3)):
-                errorfound = True
-            break  # took a long time to realise break is necessary: for example 5 7 4 3 2 could not be detected otherwise becuase the for loop would continue considering the following numbers with the expectation that they go uo.
-
-    if not errorfound:
-        result += 1
-
-print(result)
+if __name__ == '__main__':
+    result1 = solution1(lines)
+    result2 = solution2(lines)
+    f = open("solutions\solution2.txt", "w")
+    f.write(str(result1) + ": answer 1 \n")
+    f.write(str(result2) + ": answer 2 ")
+    f.close()
