62f23f1536440330204b51143ad8b261a9827346
isakstorme <isak.storme@outlook.com>
2024-12-11 16:27:46 +0100

    implemented day 11

--- day11.py
+++ day11.py
@@ -1,7 +1,10 @@
+import sys
+
 with open("input/input11.txt") as file:
     stones = list(map(int, file.read().split()))
 
 #stones= [125, 17]
+sys.setrecursionlimit(100000)
 
 
 def rules(s):
@@ -23,29 +26,31 @@ def solution1(stones):
         stones = to_add
     return len(stones)
 
-def rules2(stones):
-    result = []
-    for s in stones:
-        if s == 0:
-            result += [1]
-        s = str(s)  #str to compare length
-        if len(s) % 2 == 0:
-            half = len(s)//2
-            result += [int(s[:half]), int(s[half:])]
-        else:
-            result += [int(s) * 2024]
+cache = {}
+def helper(x, n):
+    if (x, n) in cache:
+        return cache[(x, n)]
+    if n == 0:
+        return 1
+    result = 0
+    next_stones = rules(x)
+    if len(next_stones) == 1:
+        result = helper(next_stones[0], n-1)
+    elif len(next_stones) == 2:
+        result = helper(next_stones[0], n-1) + helper(next_stones[1], n-1)
+    cache[(x, n)] = result
     return result
 
 def solution2(stones):
-    for r in range(75):
-        to_add = []
-        for i, s in enumerate(stones):
-            new_stones = rules(s)
-            to_add += new_stones
-        stones = to_add
-    return len(stones)
+    result = 0
+    for s in stones:
+        result += helper(s, 75)
+    return result
+
 
 
 if __name__ == '__main__':
     p1 = solution1(stones)
     print(p1)
+    p2 = solution2(stones)
+    print(p2)
